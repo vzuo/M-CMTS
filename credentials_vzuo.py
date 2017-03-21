a06bd086b8383437d9cbc2b67ee39bb20b9a6435
victor zuo <victor.zuo@gmail.com>
2017-03-21 13:58:00 -0600

    deleted credential files

--- credentials_vzuo.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'vzuo'
- #  account to access
-devpassword = 'eV@nB2o'#raw_input('system password: ')
-natpassword='C4e37x4F65b2'
-#  raw_input('trust host password: ')
-thpassword='Shanshan0313'
