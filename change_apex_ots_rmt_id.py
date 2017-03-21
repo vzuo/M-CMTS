fea546d0280464235666610810c4c7b31e9948c5
victor zuo <victor.zuo@gmail.com>
2017-03-21 14:05:46 -0600

    cleared confidential info

--- change_apex_ots_rmt_id.py
+++ change_apex_ots_rmt_id.py
@@ -2,13 +2,13 @@ import subprocess
 
 print "#*******************************************************************************************************************************"
 
-print "#Please verify the file like 'apex_rf_port_ots_conf_for_dx1hw_cg_slot_6_0_24channels.txt' has been generated in the output folder in APP7 server!"
+print "#Please verify the file like 'apex_rf_port_ots_conf_for_dx1ab_cd_slot_6_0_24channels.txt' has been generated in the output folder in APP7 server!"
 
 print "#Please verify the apex3000 qam name is right in above file!"
 
 print "*******************************************************************************************************************************"
 
-device_name = raw_input('Input M-CMTS name(eg. dx1hw.cg):\n').strip()
+device_name = raw_input('Input M-CMTS name(eg.):\n').strip()
 sub_slot = raw_input('Input 3G60 module sub slot number (eg.6/0):\n').strip()
 deviceName = device_name.replace('.','_')
 subslot = sub_slot.replace('/','_')
